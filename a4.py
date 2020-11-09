19d941e6ce24f246be9b90803b1554cda2ffd6c4
Noah Weingand <noahweingand@gmail.com>
2020-11-08 19:57:06 -0500

    better feature vector and clustering

--- a4.py
+++ a4.py
@@ -13,9 +13,13 @@
 
 import re
 from PorterStemmer import PorterStemmer 
+import operator
+import math
+import numpy as np
 
 tokens = []
 porter_stemmer = PorterStemmer()
+alpha = 1
 
 def clean_text(input):
     input = input.lower() # remove capitalization
@@ -49,24 +53,73 @@ def stem_tokens(tokens):
         i += 1
     return stemmed_tokens
 
-def create_feature_vector(stemmed_tokens):
-    feature_vector = {}
-    i = 0
+def create_feature_vector(stemmed_tokens, limit):
+    word_freqs = {}
     for paragraph in stemmed_tokens:
         for token in stemmed_tokens[paragraph]:
-            if token not in feature_vector:
-                feature_vector[token] = i
-                i += 1
-    return feature_vector, i
+            if token not in word_freqs:
+                word_freqs[token] = 1
+            else:
+                word_freqs[token] += 1
+    # sort frequencies in descending order
+    sorted_freqs = dict(sorted(word_freqs.items(), key=operator.itemgetter(1),reverse=True))
+    most_freq_words = {}
+    i = 0
+    for word in sorted_freqs:
+        if(i > (limit - 1)):
+            break
+        else:
+            most_freq_words[word] = i
+            i += 1
+    #print(most_freq_words)
+    return most_freq_words, i
 
 def create_tdm(stemmed_tokens, feature_vector, dimensionality):
     tdm = []
     for paragraph in stemmed_tokens:
         feature_freq = [0 for _ in range(dimensionality)]
         for token in stemmed_tokens[paragraph]:
-            feature_freq[feature_vector[token]] += 1
+            if token in feature_vector:
+                feature_freq[feature_vector[token]] += 1
         tdm.append(feature_freq)
-    return tdm
+    return np.array(tdm)
+
+
+def get_euclidean_distance(pattern, cluster_weights):
+    operand = 0
+    for i in range(len(pattern)):
+        x1 = pattern[i]
+        x2 = cluster_weights[i]
+        operand += (x1 - x2)**2
+    return math.sqrt(operand)
+
+
+# FCAN clustering
+def cluster(tdm, threshold, alpha):
+    # wk = (m * wk + alpha * X) / (m + 1)
+    all_clusters = []
+    all_clusters.append((tdm[0], 0))
+    for i in range(1, len(tdm)):
+        pattern = tdm[i]
+        closest_cluster = None
+        current_m = None
+        for j in range(len(all_clusters)):
+            current_cluster, m = all_clusters[j]
+            ed = get_euclidean_distance(pattern, current_cluster)
+            if (ed <= threshold):
+                if closest_cluster is None:
+                    closest_cluster = j
+                    current_m = m
+                elif (ed < closest_cluster):
+                    closest_cluster = j
+                    current_m = m
+        if closest_cluster is None:
+            all_clusters.append((pattern, 0))
+        else:
+            m = current_m
+            wk = ((m / (m + 1)) * closest_cluster) + ((alpha / (m + 1)) * pattern)
+            all_clusters[j] = (wk, m + 1)
+    return all_clusters
 
 # read in text files and create objects
 paragraphs_file = open("./paragraphs.txt", "r")
@@ -87,8 +140,11 @@ for paragraph in paragraphs:
 #     print(" ")
 
 stemmed_tokens = stem_tokens(tokens)
-feature_vector, dimensionality = create_feature_vector(stemmed_tokens)
+feature_vector, dimensionality = create_feature_vector(stemmed_tokens, 20)
+
 
 tdm = create_tdm(stemmed_tokens, feature_vector, dimensionality)
-print(tdm)
+#print(tdm)
+clusters = cluster(tdm, 15, alpha)
+print(len(clusters))
 
