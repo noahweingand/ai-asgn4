47ca4f074afd59b3ea9d3b08173cd7c895493df0
Noah Weingand <noahweingand@gmail.com>
2020-11-09 22:13:24 -0500

    final commit

--- a4.py
+++ a4.py
@@ -1,16 +1,3 @@
-#########################################
-# Done:
-#   - Convert upper-case to lower-case
-#   - Remove punctuation and special characters
-#   - Remove numbers
-#   - Remove stop words. A set of stop words is provided in the file “stop_words.txt”
-#   - Tokenize paragraphs
-#   - Perform stemming. Use the Porter stemming code provided in the file “Porter_Stemmer_X.txt”
-#   - Extract most frequent words.
-# To Do:
-#   - Combine stemmed words. ?????????
-#   - Cluster to group similar paragraphs together
-
 import re
 from PorterStemmer import PorterStemmer 
 import operator
@@ -26,9 +13,6 @@ def clean_text(input):
     input = input.lower() # remove capitalization
     # input = re.sub(r'\s+', ' ', input) # replace white space with a single space
     input = re.sub(r'[^\w\s]', '', input) # remove everything except words, space, and digits
-    # ^ not sure if we want to remove hypens since it messes up some words
-        # i.e. see American-Canadian in first paragraph (not sure if americancanadian should be treated
-        #       as one token [prob should])
     input = re.sub(r'\d+', '', input) # remove all digits
     return input
 
